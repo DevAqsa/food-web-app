5f827a581ffb48bcc9aae983573d60ae244286da
Aqsa <codewithaqsa@gmail.com>
2026-03-14 01:11:59 +0500

    add the templates

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,8 +6,14 @@ MEAL_TYPE = (
         ("starters", "Starters"),
         ("salads", "Salads"),
         ("main_dishes", "Main_dishes"),
-        ("desserts", "Desserts"),
+        ("desserts", "Desserts")
 )
+
+STATUS = (
+        (0 , "Unavailable"),
+        (1, "Available"),
+)
+
 
 # Create your models here.
 class Item(models.Model):
@@ -14,4 +21,12 @@ class Item(models.Model):
     description = models.CharField(max_length=2000)
     price =models.DecimalField(max_digits=10, decimal_places=2)
     meal_type = models.CharField(choices=MEAL_TYPE, max_length=100)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(default=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.meal
 
